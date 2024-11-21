afc38dc17ccf2067218fcc02cd90a729b0ef4a4f
phatsanphonna <phatsanphon2004@outlook.com>
2024-11-21 12:08:49 +0700

    feat: init shadcn component

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
 </script>
-<template>
-<h1>Hello</h1>
-</template>
+
+<template></template>
---
+++ pages/leaderboard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead>รหัสนักศึกษา</TableHead>
+        <TableHead>คะแนน</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell>65070001</TableCell>
+        <TableCell>100</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
