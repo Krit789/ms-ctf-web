edae57d10bbc31142396ba0d6a8a5e5fdffbb530
Jarukrit Sripaploen <jarukritsripaploen@protonmail.com>
2024-12-24 09:19:52 +0700

    Enhance: add text alignment to footer in default and landing layouts for improved readability

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@ import { Flag } from 'lucide-vue-next';
 
 <template>
 <NavigationBar />
-  <div class="mx-auto max-w-screen-xl pt-28 2xl:px-0 px-8">
+  <div class="mx-auto max-w-screen-xl pt-28 2xl:px-0 px-8 min-h-[84vh]">
     <slot></slot>
   </div>
-  <div class="w-full flex justify-center items-center p-8 bg-cyan-200 mt-16">
+  <div class="w-full flex justify-center items-center p-8 bg-cyan-200 mt-16 text-center">
     Made with 🩸 by @Krit789, @phatsanphonna and @OatChayanont
   </div>
 </template>
--- layouts/landing.vue
+++ layouts/landing.vue
@@ -8,7 +8,7 @@ import { Flag } from 'lucide-vue-next';
   <div class="mx-auto">
     <slot></slot>
   </div>
-  <div class="w-full flex justify-center items-center p-8 bg-cyan-200 mt-16">
+  <div class="w-full flex justify-center items-center p-8 bg-cyan-200 mt-16 text-center">
     Made with 🩸 by @Krit789, @phatsanphonna and @OatChayanont
   </div>
 </template>
